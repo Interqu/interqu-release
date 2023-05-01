3a2110d9cad123e7d9599bc487a0c4b65eaa0e77
Derek Li <42875849+rejieli@users.noreply.github.com>
2023-05-01 17:01:34 -0400

    finished login/register page

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,21 +3,58 @@ const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
 
 signUpButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
+    container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
+    container.classList.remove("right-panel-active");
 });
 
-function inputValidity() {
-  let valid = document.getElementsByClassName("field_input");
-  console.log(valid);
-  for (let i = 0; i < valid.length; i++) {
-    if (!valid[i].checkValidity()) {
-      valid.classList.add("input_error");
-    } else {
-      valid.classList.remove("input_error");
+function isValid(input) {
+    return ((input.type == "email" && !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input.value))) || input.value.trim() == '');
+}
+
+let signInForm = document.getElementById("signIn-form");
+
+signInForm.addEventListener("submit", function (e) {
+    let inputs = signInForm.getElementsByClassName("field__input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (isValid(inputs[i])) {
+            let button = signInForm.getElementsByTagName("button")[0];
+            button.classList.remove("wiggle")
+            button.offsetWidth
+            button.classList.add("wiggle")
+            e.preventDefault();
+            inputs[i].classList.add("input_error");
+        } else {
+            inputs[i].classList.remove("input_error");
+        }
     }
-  }
+})
+
+let registerForm = document.getElementById("register-form");
+
+registerForm.addEventListener("submit", function (e) {
+    let inputs = registerForm.getElementsByClassName("field__input");
+    for (var i = 0; i < inputs.length; i++) {
+        if (isValid(inputs[i])) {
+            let button = registerForm.getElementsByTagName("button")[0];
+            button.classList.remove("wiggle")
+            button.offsetWidth
+            button.classList.add("wiggle")
+            e.preventDefault();
+            inputs[i].classList.add("input_error");
+        } else {
+            inputs[i].classList.remove("input_error");
+        }
+    }
+})
+
+function inputValid() {
+    let doc = document.getElementsByClassName("field__input");
+    for (var i = 0; i < doc.length; i++) {
+        if (!isValid(doc[i])) {
+            doc[i].classList.remove("input_error");
+        }
+    }
 }
