c078295de948368bcb56115115e2e4d91ca413b0
Derek Li <rejie.li@gmail.com>
2023-05-01 11:30:01 -0400

    login page

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,11 +1,23 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const signUpButton = document.getElementById("signUp");
+const signInButton = document.getElementById("signIn");
+const container = document.getElementById("container");
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add("right-panel-active");
+signUpButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-    container.classList.remove("right-panel-active");
+signInButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
 });
+
+function inputValidity() {
+  let valid = document.getElementsByClassName("field_input");
+  console.log(valid);
+  for (let i = 0; i < valid.length; i++) {
+    if (!valid[i].checkValidity()) {
+      valid.classList.add("input_error");
+    } else {
+      valid.classList.remove("input_error");
+    }
+  }
+}
