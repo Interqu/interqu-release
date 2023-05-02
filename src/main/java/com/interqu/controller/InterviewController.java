d772f888379e0a619833b95b61a23e6243ccf449
Derek Li <rejie.li@gmail.com>
2023-05-02 10:39:35 -0400

    created camera feed

---
+++ src/main/java/com/interqu/controller/InterviewController.java
@@ -0,0 +1,19 @@
+package com.interqu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class InterviewController {
+    
+	private final String PAGE_PATH = "";
+	private final String URL_PATH = "";
+
+    // TODO disable all access feature
+    @GetMapping("dev/interview")
+    public ModelAndView interviewPractice(){
+        return new ModelAndView(PAGE_PATH + "/interview-practice");
+    } 
+
+}
